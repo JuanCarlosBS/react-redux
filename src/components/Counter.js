6b870e52930d0bed2ecbf6c3069c2e90125e0f14
juancarlosbs <juanrov_@hotmail.com>
2021-04-30 11:46:33 -0300

    feat: funcional toggle counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,30 +1,32 @@
-import { Component } from 'react';
-import { useSelector, useDispatch, connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const dispatch = useDispatch()
-  const counter = useSelector(state => state.counter);
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: 'increment' })
-  }
+    dispatch({ type: 'increment' });
+  };
 
   const increaseHandler = () => {
-    dispatch({ type: 'increase',  amount : 10 })
-  }
+    dispatch({ type: 'increase', amount: 10 });
+  };
 
   const decrementHandler = () => {
-    dispatch({ type: 'decrement' })
-  }
+    dispatch({ type: 'decrement' });
+  };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'toggle' });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Increase by 10</button>
@@ -35,7 +37,7 @@ const Counter = () => {
   );
 };
 
-export default Counter
+export default Counter;
 
 /*class Counter extends Component {
   incrementHandler() {
